2ef9ee0e4088e987bfe1bbf064f5cc49190c78de
Hu Bowen <bowenhu21@gmail.com>
2022-05-07 15:34:28 +0800

    Added int to ascii conversion

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,16 +1,14 @@
 #include "../drivers/screen.h"
+#include "../kernel/utils.h"
 
 void main() {
     clear_screen();
 
-    for (int i = 0; i < MAX_ROWS; i++) {
-        tprint("Hello World!\n");
+    for (int i = 0; i < (MAX_ROWS + 5); i++) {
+        char output[255];
+        tprint("Line ");
+        int_to_char(i, output);
+        tprint(output);
+        tprint("\n");
     }
-
-    for(int i = 0; i < 5; i++) {
-        tprint("Scroll test\n");
-    }
-
-    tprint_char('\n');
-    tprint_char('\n');
 }
--- kernel/utils.h
+++ kernel/utils.h
@@ -1,1 +1,4 @@
 void memory_copy(char* src, char* dest, int no_bytes);
+void int_to_char(int num, char arr[]);
+void reverse(char str[]);
+int strlen(char str[]);
