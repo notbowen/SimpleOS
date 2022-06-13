b5f54d4d590b002a4ca9a75308056c4021b42876
Hu Bowen <bowenhu21@gmail.com>
2022-06-13 22:02:21 +0800

    Removed border collision

--- programs/snake.c
+++ programs/snake.c
@@ -156,9 +156,8 @@ void Logic() {
             break;
     }
 
-    if (x > width|| x < 0 || y > height || y < 0) {
-        gameOver = 1;
-    }
+    if (x >= width) x = 0; else if (x < 0) x = width - 1;
+    if (y >= height) y = 0; else if (y < 0) y = height - 1;
 
     for (int i = 0;i < nTail; i++) {
         if (tailX[i] == x && tailY[i] == y) {
